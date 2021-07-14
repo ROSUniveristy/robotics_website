c7db7c8a0bb3b5a185f81b8cef79a9d0349ab28d
naju4nisa <naju4nisa@gmail.com>
2021-07-14 22:16:01 +0530

    updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 //make way for some custom css, js and images
 // app.use('/css', express.static(__dirname + '/public/assets/css'));
 // app.use('/js', express.static(__dirname + '/public/assets/js'));
-// app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/images', express.static(__dirname + '/public/images'));
 
 app.listen(app.get('PORT'), () =>
     console.log(`Server running on port ${app.get('PORT')}`),
